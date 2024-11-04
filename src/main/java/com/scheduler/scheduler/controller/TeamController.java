0159951b54a2957a2bfcdd258baae539dcc0d05e
sanga <vvfr45@naver.com>
2024-11-04 19:53:51 +0900

    2024-11-04 팀 테이블 작업중

--- src/main/java/com/scheduler/scheduler/controller/TeamController.java
+++ src/main/java/com/scheduler/scheduler/controller/TeamController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -30,18 +31,22 @@ public class TeamController {
     @GetMapping("/team/write")
     public String teamWrite(Model model) {
         List<Employee> employees = employeeService.employeeList();
+        List<Employee> employeesToRemove = new ArrayList<>();
+        // 팀에 속한 직원을 목록에서 제거하기 위한 처리
         for (Employee employee : employees) {
             Integer employeeId = employee.getEmployee_id();
-            teamMemberService.filterView(employeeId);
-            System.out.println("직원 : " + teamMemberService.filterView(employeeId));
-
+            if(teamMemberService.filterView(employeeId) != null){
+                employeesToRemove.add(employee);
+            }
         }
+        // 팀에 속한 직원을 목록에서 제거
+        employees.removeAll(employeesToRemove);
         model.addAttribute("elist", employees);
         return "teamwrite";
     }
 
     @PostMapping("/team/writepro")
-    public String teamWritePro(Team team, @RequestParam String leader) {
+    public String teamWritePro(Team team, @RequestParam String leader, @RequestParam String member) {
         teamService.write(team);
         String teamName = team.getTeamName();
         // 방금 생성된 팀명으로 팀번호 가져오기
@@ -57,6 +62,19 @@ public class TeamController {
                 teamMember.setTeam_id(teamService.view(teamName).getTeam_id());
                 teamMember.setEmployeeId(selectedEmployeeId);
                 teamMember.setRole("팀장");
+                teamMember.setMember_name(selectedEmname);
+                teamMemberService.write(teamMember);
+            }
+            // 팀원 생성
+            if(member != null){
+                String[] parts = member.split("\\|");
+                String selectedEmname = parts[0];
+                Integer selectedEmployeeId = Integer.valueOf(parts[1]);
+                // 초기화 해줌
+                TeamMember teamMember = new TeamMember();
+                teamMember.setTeam_id(teamService.view(teamName).getTeam_id());
+                teamMember.setEmployeeId(selectedEmployeeId);
+                teamMember.setRole("팀원");
                 teamMember.setMember_name(selectedEmname);
                 teamMemberService.write(teamMember);
             }
